29a2852df7c041073b193e8d7825f119cd584db4
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-09 21:04:38 +0300

    Create CollapsibleCard component and style it

---
+++ src/components/CollapsibleCard.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "../styles/CollapsibleCard.css";
+import downArrow from "../assets/down-arrow.svg"
+
+function CollapasibleCard({ children, header }) {
+
+  const [showDetails, setShowDetails] = useState(false) 
+
+  const handleCollapse = () => {
+    setShowDetails(showDetails ? false : true)
+  }
+
+  return (
+    <div className="collapsible-card">
+      <div className="heading-wrapper">
+        <h1>{header}</h1>
+        <button onClick={handleCollapse}><img className={showDetails ? 'rotated' : ''} src={downArrow} alt="down arrow icon" /></button>
+      </div>
+
+      <div 
+        className={`card-children ${showDetails ? 'shown' : ''}`}
+        >
+          <hr />
+          {children}
+        </div>
+    </div>
+  )
+}
+
+export default CollapasibleCard;
