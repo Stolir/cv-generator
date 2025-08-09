7db8ca19edaa8fb06665a929cf8ff3382ffafff3
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-09 21:17:18 +0300

    Create ContactForm component using the CollapsibleCard component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,68 @@
+import CollapasibleCard from "./CollapsibleCard";
+import FormInput from "./FormInput";
+
+function ContactForm({ userContact, handleChange, path}) {
+  return (
+    <CollapasibleCard header="Contact Information">
+      <form>
+        <FormInput 
+          fieldName={"firstName"}
+          label={"First Name"}
+          path={path}
+          type="text"
+          value={userContact.firstName}
+          placeholder="Bruce"
+          handleChange={handleChange}
+        />
+        <FormInput 
+          fieldName={"lastName"}
+          label={"Last Name"}
+          path={path}
+          type="text"
+          value={userContact.lastName}
+          placeholder="Wayne"
+          handleChange={handleChange}
+        />
+          <FormInput 
+          fieldName={"phoneNumber"}
+          label={"Phone Number"}
+          path={path}
+          type="tel"
+          value={userContact.phoneNumber}
+          placeholder="+1 234 567 890"
+          handleChange={handleChange}
+        />
+          <FormInput 
+          fieldName={"address.city"}
+          label={"City"}
+          path={path}
+          type="text"
+          value={userContact.address.city}
+          placeholder="Gotham"
+          handleChange={handleChange}
+        />
+         <FormInput 
+          fieldName={"address.country"}
+          label={"Country"}
+          path={path}
+          type="text"
+          value={userContact.address.country}
+          placeholder="United States"
+          handleChange={handleChange}
+        />
+         <FormInput 
+          fieldName={"address.postCode"}
+          label={"Postal Code"}
+          path={path}
+          type="text"
+          value={userContact.address.postCode}
+          placeholder="12345"
+          handleChange={handleChange}
+        />
+      </form>
+
+    </CollapasibleCard>
+  )
+}
+
+export default ContactForm;
