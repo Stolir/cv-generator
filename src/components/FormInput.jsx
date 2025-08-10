2e2db72cb201c1807cd6fe5244c7c718e2d4d25d
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-10 18:16:57 +0300

    Add necessary props for array data and use getPath with handleChange

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,19 @@
 import "../styles/FormInput.css"
+import { getPath } from "../utils/helper";
 
-function FormInput({fieldName, label, handleChange, path, type, value, placeholder }) {
+function FormInput({fieldName, label, handleChange, path, type, value, placeholder, index = null, readOnly = false }) {
+
   return (
     <div className={`input-wrapper ${fieldName}`}>
       <label>{label}</label>
       <input 
-        type={type} 
+        {...(index && {index})}
+        type={type}
+        value={value}
         placeholder={placeholder}
+        readOnly={readOnly}
         onChange={(e) => {
-          handleChange(`${path}.${fieldName}`, e.target.value)
+          handleChange(getPath(path, fieldName, index), e.target.value)
         }}
         />
     </div>
