fa287106f74ab184190c8393f6bac0093cc92f64
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-14 00:55:29 +0300

    Create and style EditPanelCard component to be used in EditPanel

---
+++ src/components/EditPanelCard.jsx
@@ -0,0 +1,23 @@
+import { formatDate } from "../utils/helper"
+import editButtonBlack from "../assets/edit-black.svg"
+import trashButtonBlack from "../assets/trash-black.svg"
+import "../styles/EditPanelCard.css"
+
+function EditPanelCard({data}) {
+  const date = `${formatDate(data.startDate)} - ${data.endDate === "Current" ? data.endDate : formatDate(data.endDate)}`
+  return (
+    <div className="card">
+      <div className="information">
+        <p className="title">{data.jobTitle}</p>
+        <p className="company">{data.company}</p>
+        <p className="date">{date}</p>
+      </div>
+      <div className="controls">
+        <button><img src={editButtonBlack} alt="edit icon" /></button>
+        <button><img src={trashButtonBlack} alt="trash icon" /></button>
+      </div>
+    </div>
+  )
+} 
+
+export default EditPanelCard;
